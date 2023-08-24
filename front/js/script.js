5bb4138fd3e53bab358304c9c2af873f9fccd28b
nadjiZz <115700348+nadjiZz@users.noreply.github.com>
2023-08-24 14:39:29 +0100

    commit du 24/08

--- front/js/script.js
+++ front/js/script.js
@@ -1,22 +1,25 @@
-const url = "http://localhost:3000/api/products/"
-const container = document.getElementById("items")
+// récupère les produits et les affichent sur la page d'accueil
 
-
-const getArticles = () => {
-    fetch(url)
-    .then(function(res){
-        return res.json()
-    })
-    .then(function(data){
-        console.log(data)
-        for(product in data) {
-            container.innerHTML += `<a href="./product.html?id=42">
-            <article>
-              <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-              <h3 class="productName">Kanap name1</h3>
-              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-            </article>
-          </a>`
-        }
-    })
+//
+async function addArticles() {
+    await fetch('http://localhost:3000/api/products')
+        .then((res) => res.json())
+        .then((data) => {
+            for (let cpt = 0; cpt < data.length; cpt++) {
+                element = data[cpt];
+                document.querySelector('#items').innerHTML += `
+                    <a href="./product.html?id=${element._id}">
+                        <article>
+                            <img src="${element.imageUrl}" alt="${element.altTxt}">
+                            <h3 class="productName">${element.name}</h3>
+                            <p class="productDescription">${element.description}</p>
+                        </article>
+                    </a>`;
+            }
+        })
+        .catch((error) => {
+            console.log('Erreur de connexion avec le serveur : ', error);
+            window.alert('Connexion au serveur impossible !');
+        });
 }
+        addArticles();
