1261ee8bcc394525362c24a690366d8958308d14
nadjiZz <115700348+nadjiZz@users.noreply.github.com>
2023-09-05 22:57:30 +0100

    Update confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,8 +1,1 @@
-// Récupère l'ordreId de l'URL
-let orderId = new URLSearchParams(window.location.search).get('orderId');
 
-// Met le N° de commande dans la zone HTML réservé à l'affichage
-document.getElementById('orderId').textContent = orderId;
-
-// Vide le localStorage
-window.localStorage.clear();
